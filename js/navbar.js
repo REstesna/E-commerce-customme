ec4981b804fd9c5655083e00c9ce09f0ee43d1f1
REstesna <mohammad.allahyari.am@gmail.com>
2025-09-25 19:49:40 +0330

    start category and fix details in index.html

--- js/navbar.js
+++ js/navbar.js
@@ -8,9 +8,12 @@ const searchInputElem = $.querySelector('#search-input')
 // active search type by click on search-box
 searchBoxElem.addEventListener('click', () => {
     searchBoxElem.classList.add('active-search');
-    searchInputElem.focus()
+    searchInputElem.focus();
+    
 });
 
 searchInputElem.addEventListener('blur', () => {
     searchBoxElem.classList.remove('active-search');
-})
+});
+
+
